6a549bdb1464e1b7a38dccea35a755e478de9bb8
Anthony Barberis <skamunism@gmail.com>
2019-09-13 23:54:34 -0700

    Added mouse coordinate listener

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-window.onload = init();
+window.onload = init;
 
+function init() {
+    document.getElementById('coords').addEventListener('mouseover', function(event) {
+        this.innerHTML = event.clientX
+    })
+}
